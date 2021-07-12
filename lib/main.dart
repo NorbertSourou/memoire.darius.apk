ac52fa5f4bc052615e35153554eb73a8ec54a695
Norbert Souwouin <souwouinnorbert@gmail.com>
2021-07-12 14:33:58 +0100

    Mise en place du projet mobile

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:memoire/src/views/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/src/views/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
